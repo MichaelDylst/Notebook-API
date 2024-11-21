7930a3c612adbc7b70cfd64840688aa95d9f64bc
Michael Dylst <michael.dylst@icloud.com>
2024-11-21 11:56:26 +0100

    made the index.js for making a basic server

---
+++ index.js
@@ -0,0 +1,19 @@
+// checken of een server nog in gebruik is: 
+// lsof -i :3000
+// server afsluiten
+// kill -9 <PID> -> PID is het servernummer
+
+const express = require("express")
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// eenvoudige route 
+
+app.get('/', (req, res) => {
+    res.send('Whats up server!')
+});
+
+app.listen(PORT, () => {
+    console.log(`Server draait op https://localhost:${PORT}`);
+})
