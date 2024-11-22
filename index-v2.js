80ef3a1cd0cbc76460256b87c43dea5af30bc04b
Michael Dylst <michael.dylst@icloud.com>
2024-11-22 09:44:17 +0100

    code functioning fully.

--- index-v2.js
+++ index-v2.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 
 
 // necessary variables 
-const express = require('express');
-const cors = require('cors')
+const express = require('express'); // import express module
+const cors = require('cors')    // import cors 
 const app = express();
 const { Client } = require('pg')
 const dbPass = process.env.DB_PASS;
 const PORT = process.env.PORT;
 
-let client = Client({
+let client = new Client({
     user: 'postgres',
     host: 'localhost', 
     database: 'Notebook_API',
@@ -23,9 +23,18 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    console.log("Hello, this server is currently running on your macbook. Nice.")
+    res.send("Hello, this server is currently running on your macbook. Nice.")
 });
 
 app.listen(PORT, () => {
     console.log(`Server started running on port: ${PORT}`)
-})
+});
+
+app.post('/submit', async (req, res)=>{
+    const {title, description} = req.body;
+    const query = 'INSERT INTO notebook(title, description) VALUES($1, $2) RETURNING *';
+    const values = [title, description];
+    const result = await client.query(query,values);
+
+    res.json({ message: "Data successfully received!" }); // Express response
+});
